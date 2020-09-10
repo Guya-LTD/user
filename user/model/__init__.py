72cac578e4533b959761a53c3d0205c50591d4fd
Simonbelete <simonbelete@gmail.com>
2020-09-10 05:06:36 +0300

    fix(migration): adding permission migration model for pytest

--- user/model/__init__.py
+++ user/model/__init__.py
@@ -2,10 +2,12 @@ from .user import User
 from .credential import Credential
 from .role import Role
 from .user_role import UserRole
+from .permission import Permission
 
 __all__ = [
     'User',
     'Credential',
     'Role',
-    'UserRole'
+    'UserRole',
+    'Permission'
 ]
